e29ddb5881f3054421f772c2b4fb4f3f71ff553e
Cristiano Castiel <cristianoct03@gmail.com>
2024-03-14 17:33:31 -0300

    Project Initialized

---
+++ Rascunho/PrintMatrix.cs
@@ -0,0 +1,68 @@
+﻿namespace Rascunho
+{
+    internal static class PrintMatrix
+    {
+
+        public static string[,] GenerateStringMatrix(int[,] mat)
+        {
+            int largestNumber = GetLargestLengthNumber(mat);
+            string[,] stringMat = ConvertMatrixToString(mat);
+
+            for(int i = 0; i < mat.GetLength(0); i++)
+            {
+                for(int j = 0; j < mat.GetLength(1); j++)
+                {
+                    int number = int.Parse(stringMat[i, j]);
+                    stringMat[i, j] = EqualizingPlace(number, largestNumber, '0');
+                }
+            }
+            return stringMat;
+        }
+        private static int GetLargestLengthNumber(int[,] mat)
+        {
+            int largestNumber = 0;
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if (mat[i, j].ToString().Length > largestNumber) largestNumber = mat[i, j].ToString().Length;
+                }
+            }
+
+            return largestNumber;
+        }
+
+        private static string[,] ConvertMatrixToString(int[,] mat)
+        {
+            string[,] sMat = new string[mat.GetLength(0), mat.GetLength(1)];
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    sMat[i, j] = mat[i, j].ToString();
+                }
+            }
+
+            return sMat;
+        }
+
+        private static string EqualizingPlace(int number, int quantity, char character)
+        {
+            try
+            {
+                if (number.ToString().Length > quantity)
+                {
+                    throw new Exception("The number connot be greater than quantity value");
+                }
+                return new string(character, quantity - number.ToString().Length) + number.ToString();
+            }
+            catch (Exception ex)
+            {
+                return (ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Rascunho/Program.cs
@@ -0,0 +1,35 @@
+﻿using Rascunho;
+
+Console.Write("Inform the value to start a new matrix (NxN): ");
+int quantity = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the velues of the matrix:");
+int[,] mat = new int[quantity, quantity];
+
+try
+{
+    for (int i = 0; i < quantity; i++)
+    {
+        for (int j = 0; j < quantity; j++)
+        {
+            Console.WriteLine($"Row {i + 1}, Column {j + 1}");
+            mat[i, j] = int.Parse(Console.ReadLine());
+        }
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+string[,] stringMat= PrintMatrix.GenerateStringMatrix(mat);
+for (int i = 0; i < stringMat.GetLength(0); i++)
+{
+    for (int j = 0; j < stringMat.GetLength(1); j++)
+    {
+        Console.WriteLine(" " + stringMat[i, j]);
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
+
